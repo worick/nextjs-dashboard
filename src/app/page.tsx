ceb18990029b30b08abfc54780de5459bc9017b3
Woniksu <worick@gmail.com>
2025-01-19 23:40:45 +0900

    테스트 문장추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
           <li>Can you see?</li>
+          <li>My name is worick</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
